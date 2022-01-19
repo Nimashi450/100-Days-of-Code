36894873b15f180a55249d57fdebf11c7b932a34
bmnimashi@gmail.com <bmnimashi@gmail.com>
2022-01-20 00:17:37 +0530

    add features to app

--- Day01/index.js
+++ Day01/index.js
@@ -3,13 +3,23 @@
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 let count = 0
+let countEl = document.getElementById("count-el")
 function increment() {
     count = count + 1
-    document.getElementById("count-el").innerText = count
+    countEl.innerText = count
     console.log("the button was clicked")
 }
 
 function remove() {
     count = count - 1
-    document.getElementById("count-el").innerText = count
+    countEl.innerText = count
+}
+
+let saveEl = document.getElementById("save-el")
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0 
+    console.log(count)
 }
