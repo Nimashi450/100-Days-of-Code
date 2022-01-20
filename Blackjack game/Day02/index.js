2ac546af06882014b2412652445332cfe0ebf107
bmnimashi@gmail.com <bmnimashi@gmail.com>
2022-01-21 00:04:07 +0530

    day02

--- Blackjack game/Day02/index.js
+++ Blackjack game/Day02/index.js
@@ -3,26 +3,35 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = Math.floor(Math.random() * 11) + 2;
-let secondCard = Math.floor(Math.random() * 11) + 2;
 let sum 
 let hasBlackJack = false
 let isAlive = true
 
-sum = firstCard + secondCard
-console.log(firstCard)
-console.log(secondCard)
-console.log(sum)
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    hasBlackJack = true
-    console.log("Wohoo! You've got Blackjack! 🥳")
-} else {
-    isAlive = false
-    console.log("You're out of the game! 😭")
+
+let sumEl = document.querySelector("#sum-el")
+let messageEl = document.getElementById("message-el")
+
+function startGame() {
+    let firstCard = Math.floor(Math.random() * 11) + 2;
+    let secondCard = Math.floor(Math.random() * 11) + 2;
+    sum = firstCard + secondCard
+
+
+    if (sum <= 20) {
+        messageEl.innerText = "Do you want to draw a new card? 🙂"
+        sumEl.innerText = "Sum: " + sum
+        
+    } else if (sum === 21) {
+        hasBlackJack = true
+        messageEl.innerText = "Wohoo! You've got Blackjack! 🥳"
+        sumEl.innerText = "Sum: " + sum
+    } else {
+        isAlive = false
+        messageEl.innerText = "You're out of the game! 😭"
+        sumEl.innerText = "Sum: " + sum
+    }
 }
 
-console.log(hasBlackJack)
-console.log(isAlive)
+
+
